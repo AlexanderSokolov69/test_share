24d7838ae19a7d9e1979c6be5152106d7179f850
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2026-03-23 12:08:00 +0300

    First0

--- main.py
+++ main.py
@@ -127,7 +127,9 @@ def bad_request(_):
     return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 def main():
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=5050, debug=True)
 
 
 if __name__ == '__main__':
