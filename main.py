de1f8d520810eec87d4dc05f03f7a75398cc6759
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2026-03-23 22:10:53 +0300

    all

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ def bad_request(_):
     return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 def main():
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5050))
     app.run(host='0.0.0.0', port=port)
     # app.run(host='0.0.0.0', port=5050, debug=True)
 
